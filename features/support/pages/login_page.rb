bd5104823047253c8235dbd6c3e6ec9d3831853d
Felix Mason <sparrows.go@gmail.com>
2019-03-11 23:22:48 +0000

    parameterize login_as function

--- features/support/pages/login_page.rb
+++ features/support/pages/login_page.rb
@@ -3,21 +3,21 @@ class LoginPage
   include Capybara::DSL
   
   def email_input
-    page.find('#login_email')
+    find('#login_email')
   end
   
   def pw_input
-    find_field(id: 'login_password')
+    find('#login_password')
   end
   
   def login_button
-    find_button(name: 'commit')
+    find('[name=commit]')
   end
 
-  def login_as()
-    email_input.set('not@anemail')
-    pw_input.set('1234567')
+  def login_as(email, password)
+    email_input.set(email)
+    pw_input.set(password)
     login_button.click
-    
   end
+
 end
